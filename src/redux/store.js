019249e726d9da0dafb2ecadf9522a6b03726a67
Luca Montaigut <lmontaigut@gmail.com>
2020-05-29 11:35:46 +0200

    fix devtool compose

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,11 +12,12 @@ const logger = (store) => {
     }
 }
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     reducer,
-    compose(
-        applyMiddleware(logger),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    composeEnhancers(applyMiddleware(logger))
 )
+
 
 export default store
